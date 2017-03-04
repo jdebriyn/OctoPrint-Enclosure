4c67ea845bc0599b41fdf0c9d3d208e1ec4d7522
jdebriyn <jdebriyn@gmail.com>
2017-03-04 13:51:42 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "enclosure"
+plugin_identifier = "enclosure2"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_enclosure"
+plugin_package = "JDD_octoprint_enclosure"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-Enclosure"
+plugin_name = "JDDOctoPrint-Enclosure"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.5"
+plugin_version = "1.5x"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
@@ -27,7 +27,7 @@ plugin_author = "Vitor Henrique"
 plugin_author_email = "vitormhenrique@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/vitormhenirque/OctoPrint-Enclosure"
+plugin_url = "https://github.com/jdebriyn/OctoPrint-Enclosure"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
